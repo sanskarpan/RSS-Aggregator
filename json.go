359e9e921f2e8a7682d64ba9a934a082a3b1ac34
sanskarpan <sanskarpandey2004@gmail.com>
2025-09-20 02:38:03 +0530

    Refactor JSON response handling in `json.go`
    - Improved error logging in `respondWithError` to include HTTP status code in messages.
    - Streamlined JSON marshaling in `respondWithJSON`, enhancing clarity with comments on the process.
    - Ensured proper response header and status code handling for JSON responses.

--- json.go
+++ json.go
@@ -7,26 +7,32 @@ import (
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
+	//errors 500 and above are from the server end thats why we only check that
+	//we dont care about slient side errors (< 500)
 	if code > 499 {
-		log.Printf("Server error: %v", msg)
-	}
-	type errResponse struct {
-		Error string `json:"error"`
+		log.Printf("Responding with %v error: %v", code, msg)
 	}
 
-	respondWithJSON(w, code, errResponse{
-		Error: msg,
-	})
+	type errResponse struct {
+		Error string `json:"error"` //=> {"error" : "msg"}
+	}
+
+	respondWithJSON(w, code, errResponse{Error: msg})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	dat, err := json.Marshal(payload)
+	//converting the payload to json
+	data, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Failed to marshal JSON response: %v", payload)
 		w.WriteHeader(500)
 		return
 	}
+
+	//after the marshaling succeeded, we tell that we are writing with JSON format
 	w.Header().Add("Content-Type", "application/json")
+	//we set a status code of the passed in response code to indicate success
 	w.WriteHeader(code)
-	w.Write(dat)
+	//we write the data
+	w.Write(data)
 }
