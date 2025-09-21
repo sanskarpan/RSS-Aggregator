664cf1838d539b750f71199cf531447ac6a9eb1a
sanskarpan <sanskarpandey2004@gmail.com>
2025-09-21 14:49:32 +0530

    Implement database connection and enhance API routing in `main.go`
    - Established a connection to the PostgreSQL database using environment variables for configuration.
    - Introduced a new `apiConfig` struct to manage database queries.
    - Added new HTTP handlers for user and feed management, including user creation, retrieval, and feed follow operations.
    - Updated CORS settings to improve security and response handling.
    - Initiated a background scraping process to run concurrently with the server.

--- main.go
+++ main.go
@@ -1,37 +1,90 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"log"
-	"os"
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/go-chi/cors"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/itsemadbattal/rss-aggregator/internal/database"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
+// _ means include this code in my program even though im not calling it directly
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
+	// feed, err := urlToFeed("https://wagslane.dev/index.xml")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(feed)
+
+	// fmt.Println("Hello World")
+
+	//load the .env file
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
-		log.Fatal("PORT is not found in the environment")
+		log.Fatal("PORT NOT FOUND IN THE .env")
 	}
 
-	router := chi.NewRouter() 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL NOT FOUND IN THE .env")
+	}
+	//connecting to our db
+	conn, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Fatal("Cannot connect to databse.")
+	}
+
+	//we need to convert to conn to our package so we use database.New()
+	db := database.New(conn)
+	apiCfg := apiConfig{
+		DB: db,
+	}
+
+	// starting the scraping on a new goroutine
+	go startScraping(db, 10, time.Minute)
+
+	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"*"},
-		ExposedHeaders: []string{"Link"},
-		AllowCredentials: true,
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
 	}))
 
 	v1Router := chi.NewRouter()
-	v1Router.Get("/healthz",handlerReadiness)
-	v1Router.Get("/err",handlerErr)
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Get("/users", apiCfg.handlerGetUserByName)
+
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+
+	v1Router.Post("/feed-follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed-follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+
+	v1Router.Delete("/feed-follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
 
 	router.Mount("/v1", v1Router)
 
@@ -40,11 +93,10 @@ func main() {
 		Addr:    ":" + portString,
 	}
 
-	log.Printf("Server started on port %v", portString)
-	err := srv.ListenAndServe()
+	log.Printf("Server starting on port %v", portString)
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Port: ", portString)
 }
